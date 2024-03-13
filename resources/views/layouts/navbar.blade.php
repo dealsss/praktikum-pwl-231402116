a0c16fee999473c7ac859322e8964b223f283319
dealsss <fadhilharits29@gmail.com>
2024-03-13 14:38:43 +0700

    first commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+  </div>
+  <div class="navbar-center">
+    <a href="/" class="btn btn-ghost text-xl hover:bg-transparent">To Do List</a>
+  </div>
+  <div class="navbar-end">
+  </div>
+</div>
+</body>
+</html>
